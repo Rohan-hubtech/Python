7365d6af9f04048e983daa539ff38a8f9bd6a9a2
Rohan-hubtech <rohanyarlagadda10@gmail.com>
2024-09-28 16:35:43 +0100

    country

---
+++ Abstraction/capital.py
@@ -0,0 +1,22 @@
+class India():
+    def captial(self):
+        print("New Delhi is the capital of india.")
+
+    def language(self):
+        print("Hindi is the most widely spoken language")
+
+    def type(self):
+        print("India is a developing country")
+
+class USA():
+    def capital(self):
+        print("Washington, D.C is the capital of the USA.")
+
+    def language(self):
+        print("English is the primary language of the USA")
+
+    def type(self):
+        print("Use is a devoloped country")
+
+obj_ind = India()
+obj_usa = USA()
